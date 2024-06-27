971d47eb6ca4175222dcbde81cb30f196e8e6f6d
Hengmao Yue <hengmaoyue@gmail.com>
2024-06-27 09:59:59 +0800

    update location

--- interface.py
+++ interface.py
@@ -1,14 +1,14 @@
 #选择框的左上角点位置
-x0_ratio = 2/8 #可以更改
-y0_ratio = 4/8 #可以更改
+x0_ratio = 2.5/8 #可以更改
+y0_ratio = 3.5/8 #可以更改
 #选择框的右下角点位置
-x1_ratio = 7/8 #可以更改
-y1_ratio = 8/9 #可以更改
+x1_ratio = 6/8 #可以更改
+y1_ratio = 7/9 #可以更改
 #获取线性公式设置为Flase，获取识别结果设置为True
-con_detect =True #可以更改
+con_detect =False #可以更改
 #获取线性公式时，需要手动输入浓度值，个数一定严格和比色皿数量比配
-con_list = [30.7, 10, 50.9, 80, 1, 100, 120] #可以更改
-b_avg_list = []
+con_list = [1, 2.5, 5, 7.5, 10, 20, 30, 40] #可以更改
+# b_avg_list = []
 
 # Project存放位置： C:\Users\李肖夏\Desktop\Peng\Peng\
 # 原始照片存放位置： C:\Users\李肖夏\Desktop\Peng\Peng\custom\detectImg\Peng
