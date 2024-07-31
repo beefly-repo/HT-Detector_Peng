a153afaa1d3e7a08c4018d0bfdedb999aba29baf
Hengmao Yue <hengmaoyue@gmail.com>
2024-07-31 17:36:48 +0800

    comment

--- ultralytics/utils/plotting.py
+++ ultralytics/utils/plotting.py
@@ -193,6 +193,7 @@ class Annotator:
                 outside = p1[1] - h >= 3
                 p2 = p1[0] + w, p1[1] - h - 3 if outside else p1[1] + h + 3
                 cv2.rectangle(self.im, p1, p2, color, -1, cv2.LINE_AA)  # filled
+                # print('my label:', label)
                 cv2.putText(
                     self.im,
                     label,
