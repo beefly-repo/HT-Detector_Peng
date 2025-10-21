38be60c8d498c35d56e6ff28d27e3583e6bfce1d
beefly-repo <hungry_foolish_ing@163.com>
2025-10-21 17:02:32 +0900

    Update interface.py

--- interface.py
+++ interface.py
@@ -15,7 +15,7 @@ y1_ratio = 5.6/9 #可以更改
 #获取线性公式设置为 linear，获取识别结果设置为 detection
 mode = 'detection' # 'linear' or 'detection' 可以更改只能选择这两个之一
 #获取线性公式时，需要手动输入浓度值，个数一定严格和比色皿数量比配
-con_list = [1, 5, 10, 20, 30, 40] #可以更改
+con_list = [1, 10, 20, 30, 40] #可以更改
 #计算RGB平均值用到的小数点位数，如不提供程序默认16位
 rgb_calculate_accuracy = 16 #可以更改
 #图片显示RGB和Con.的小数点位数，可以单独控制
